faedb42a997daea5ed69eef6b8da6aefb0f5138c
JonasCS16 <jonascs1692@gmail.com>
2025-04-06 12:29:14 -0500

    Add files via upload

---
+++ data/DOMOS_9.js
@@ -0,0 +1,11 @@
+var json_DOMOS_9 = {
+"type": "FeatureCollection",
+"name": "DOMOS_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": "H1D5.2" }, "geometry": { "type": "Point", "coordinates": [ -79.978633001405342, -4.023661939968697 ] } },
+{ "type": "Feature", "properties": { "id": null, "ETIQUETA": null }, "geometry": { "type": "Point", "coordinates": [ -79.981620215008604, -4.098837155891298 ] } },
+{ "type": "Feature", "properties": { "id": "7", "ETIQUETA": "H1D5" }, "geometry": { "type": "Point", "coordinates": [ -80.016513745894898, -4.019521103726099 ] } },
+{ "type": "Feature", "properties": { "id": "8", "ETIQUETA": "H1D5.1" }, "geometry": { "type": "Point", "coordinates": [ -79.992735820728697, -4.021725441796495 ] } }
+]
+}
